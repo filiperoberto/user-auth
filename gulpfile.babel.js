b6ae2ebea8704766fa1ee6e5a5d8e24441ea2733
filiperoberto <filiperoberto.s@gmail.com>
2016-11-28 10:02:25 -0200

    Adding more usefull tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const runSequence = require('run-sequence');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
+const fs = require('fs');
 
-// pull in the project TypeScript config
 const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('scripts', () => {
@@ -10,8 +13,29 @@ gulp.task('scripts', () => {
   return tsResult.js.pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', ['scripts'], () => {
+gulp.task('watch', ['build'], () => {
   gulp.watch('src/**/*.ts', ['scripts']);
 });
+
+gulp.task('clean',() => {
+	gulp.src('dist',{read : false}).pipe(clean());
+});
+
+gulp.task('build',done => {
+	runSequence('clean','scripts','env',done);
+})
+
+gulp.task('env',() => {
+
+	fs.stat('.env',(err, stat) => {
+    	if(err == null) {
+    		gulp.src('.env').pipe(gulp.dest('dist'));
+    	} else {
+        	gulp.src('.env_sample')
+        		.pipe(rename('.env'))
+        		.pipe(gulp.dest('dist'));
+    	}
+	});
+})
 
 gulp.task('default', ['watch']);
