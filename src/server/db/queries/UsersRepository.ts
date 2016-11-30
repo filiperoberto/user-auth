1542800d86de1c41bb952ee24b6edf721e0431a7
Filipe Silva <filiperoberto.s@gmail.com>
2016-11-29 22:17:35 -0200

    Fixing hash algoritm

--- src/server/db/queries/UsersRepository.ts
+++ src/server/db/queries/UsersRepository.ts
@@ -1,6 +1,6 @@
 import * as Knex from 'knex';
 const knex : Knex = require('../Connection');
-const bcrypt = require('bcryptjs');
+const sha1 = require('sha1');
 
 export class UsersRepository {
 
@@ -9,9 +9,10 @@ export class UsersRepository {
     }
 
     public getUserByUsernameAndPassword(username : string, password : string) {
-        //const hash = bcrypt.hashSync(password, 8);
-        //console.log(hash);
-        return knex.select().from('ck_users').where({'username' : username, 'password' : password}).limit(1);
+
+        const salt = 'rzl5wxovlzbjlcvljl1vuxuyaxrhfpvkq58pvdzg';
+        const hash = sha1(salt+password);
+        return knex.select().from('ck_users').where({'username' : username, 'password' : hash}).limit(1);
     }
 
 }
