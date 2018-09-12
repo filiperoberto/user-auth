8527b334eb082497467dd51b045945bf809c8975
Filipe Silva <frsilva@ciasc.sc.gov.br>
2018-09-12 13:50:36 -0300

    Colcoando pai e mae na consulta por pessoa

--- src/server/db/queries/PeopleRepository.ts
+++ src/server/db/queries/PeopleRepository.ts
@@ -7,7 +7,9 @@ const resultMaps = [
         mapId: 'personMap',
         idProperty: 'id',
         collections: [
-            {name : 'version', mapId: 'versionMap', columnPrefix: 'ck_versions_'}
+            {name : 'version', mapId: 'versionMap', columnPrefix: 'ck_versions_'},
+            {name : 'pai', mapId: 'versionMap', columnPrefix: 'pai_'},
+            {name : 'mae', mapId: 'versionMap', columnPrefix: 'mae_'},
         ]
     },
     {
@@ -21,15 +23,11 @@ export class PeopleRepository {
 
     private attributes: string[] = ['id', 'version_number', 'citacoes', 'descricao', 'sinonimos', 'created', 'modified', 'aprovada', 'id_pessoa', 'user_id', 'idade_morte', 'idade_pai_nascimento', 'idade_mae_nascimento', 'sexo', 'linhagem_de_jesus', 'nome', 'rei', 'profeta', 'sacerdote', 'juiz', 'pai', 'mae'];
 
-    /*public getAll() {
-        let query = this.getPersonQuery(); 
-        return knex.raw(query)
-    }*/
-
     public getById(id: string) {
 
         let selectedAttributes = this.getAttributes('ck_versions');
         selectedAttributes = selectedAttributes.concat(this.getAttributes('pai'));
+        selectedAttributes = selectedAttributes.concat(this.getAttributes('mae'));
         selectedAttributes.push('ck_pessoas.id')
 
         return knex('ck_pessoas')
@@ -37,54 +35,42 @@ export class PeopleRepository {
             .innerJoin('ck_versions','ck_versions.id_pessoa','ck_pessoas.id')
             .leftOuterJoin(
                 knex('ck_versions as p')
-                .select()
-                .whereIn('p.id',function() {
-                    this.max('ck_versions.id')
-                    .from('ck_versions')
-                    .where('ck_versions.id_pessoa','p.id')
-                    .andWhere('ck_versions.aprovada',1)
-                })
+                .select('p.*')
+                .innerJoin(
+                    knex('ck_versions as ppp')
+                        .select(knex.raw('max(ppp.id) as ppp_id'),'ppp.id_pessoa')
+                        .where('ppp.aprovada',1)
+                        .groupBy('ppp.id_pessoa')
+                        .as('ppp')
+                    ,'p.id'
+                    ,'ppp_id'
+                )
                 .as('pai')
-                ,'ck_versions.pai','pai.id')
+                ,'ck_versions.pai','pai.id_pessoa')
+            .leftOuterJoin(
+                knex('ck_versions as p')
+                .select('p.*')
+                .innerJoin(
+                    knex('ck_versions as ppp')
+                        .select(knex.raw('max(ppp.id) as ppp_id'),'ppp.id_pessoa')
+                        .where('ppp.aprovada',1)
+                        .groupBy('ppp.id_pessoa')
+                        .as('ppp')
+                    ,'p.id'
+                    ,'ppp_id'
+                )
+                .as('mae')
+                ,'ck_versions.mae','mae.id_pessoa')
             .whereIn('ck_versions.id',function() {
                 this.max('ck_versions.id')
                 .from('ck_versions')
                 .where('ck_versions.id_pessoa',id)
                 .andWhere('ck_versions.aprovada',1)
             })
-            .where('ck_pessoas.id',id)/*.then(resultSet => {
+            .where('ck_pessoas.id',id).then(resultSet => {
                 return joinjs.map(resultSet, resultMaps, 'personMap');
-            });*/
+            });
     }
-
-    /*private getPersonQuery(): string {
-
-        let versionAttributes = this.getAttributes('v');
-        let paiAttributes = this.getAttributes('pai');
-        let maeAttributes = this.getAttributes('mae');
-
-        return `SELECT ${versionAttributes.join(',')}, ${paiAttributes.join(',')}, ${maeAttributes.join(',')}
-            FROM ck_pessoas pe
-                        INNER jOIN  ck_versions v
-                        ON v.id = pe.last_version
-                        LEFT OUTER JOIN (
-                            SELECT pv.*
-                            FROM ck_pessoas ppe
-                            INNER jOIN  ck_versions pv
-                            ON pv.id = ppe.last_version
-                            WHERE pv.aprovada = 1
-                        ) AS pai
-                        ON pai.id_pessoa = v.pai
-                        LEFT OUTER JOIN (
-                            SELECT mv.*
-                            FROM ck_pessoas mpe
-                            INNER jOIN  ck_versions mv
-                            ON mv.id = mpe.last_version
-                            WHERE mv.aprovada = 1
-                        ) AS mae
-                        ON mae.id_pessoa = v.mae
-                        WHERE v.aprovada = 1`
-    }*/
 
     private getAttributes(prefix: string): string[] {
         let clone = this.attributes.slice(0);
